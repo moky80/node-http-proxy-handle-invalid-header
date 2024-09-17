8d460432ed1ee6b24c5985ccd353799dfeae219a
moky80 <kaimi2005@yahoo.com>
2024-09-17 13:50:12 +0200

    Adding error handling to proxy server does not help

--- proxy-server.js
+++ proxy-server.js
@@ -1,11 +1,14 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
-
 const TARGET_SERVER = 'http://localhost:8888'; // The main server
 
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: TARGET_SERVER });
+  proxy.web(req, res, { target: TARGET_SERVER }, function(err) {
+    console.error('Proxy error:', err);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Something went wrong while proxying the request.');
+  });
 });
 
 const PORT = 4444;
