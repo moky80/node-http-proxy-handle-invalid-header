d4007c0480c7d98e55852dc534f6eb683fb9640c
moky80 <kaimi2005@yahoo.com>
2024-09-18 15:27:46 +0200

    Test proxy events 'error', 'open', 'data', 'proxyReq'

--- proxy-server.js
+++ proxy-server.js
@@ -3,12 +3,38 @@ const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const TARGET_SERVER = 'http://localhost:8888'; // The main server
 
-const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: TARGET_SERVER }, function(err) {
-    console.error('Proxy error:', err);
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('Something went wrong while proxying the request.');
+// The event 'error', 'open', 'data' are documented in https://github.com/http-party/node-http-proxy#
+// but were never observed in my tests.
+proxy.on('error', function(e) {
+  console.error('--proxy error:', err);
+  res.writeHead(500, { 'Content-Type': 'text/plain' });
+  res.end('Something went wrong while proxying the request.');
+});
+
+proxy.on('open', function (proxySocket) {
+  console.log("--proxy open");
+  // listen for messages coming FROM the target here
+  proxySocket.on('data', function(arg1, arg2){
+    console.log("proxy data:", arg1);
   });
+});
+
+// This happens when proxy does not crash and can proceed to receive the request.
+// The exception ERR_INVALID_HTTP_TOKEN happens before this event.
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  try {
+    // Example: Validate headers
+    console.log("--proxy req.headers:", req.headers);
+    if (req.headers['User Agent']) {
+        throw new Error('Invalid header');
+    }
+  } catch (err) {
+      proxyReq.emit('error', err);
+  }
+});
+
+const server = http.createServer((req, res) => {
+  proxy.web(req, res, { target: TARGET_SERVER });
 });
 
 const PORT = 4444;
--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 # Valid header works for both main-server and proxy-server
 curl -v -H "User-Agent: pinkytinky" http://localhost:8888
+curl -v -H "User-Agent: pinkytinky" http://localhost:4444
 
 # Invalid header works for main-server but crashes proxy-server
-curl -v -H "User Agent: pinkytinky" http://localhost:8888
+curl -v -H "User Agent: pinkytinky" http://localhost:4444
 
 # The crash log: see crash.log
